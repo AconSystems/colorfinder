0f2963f39e558cf85eae1f0aea331fd31d4b8972
AconSystems <acon01@gmx.at>
2026-01-29 00:07:19 +0100

    Update sw.js

--- daten-desktop/sw.js
+++ daten-desktop/sw.js
@@ -20,7 +20,10 @@ self.addEventListener("install", event => {
 self.addEventListener("activate", event => {
   event.waitUntil(
     caches.keys().then(keys => {
-      return Promise.all(keys.map(key => caches.delete(key)));
+      return Promise.all(
+        keys.filter(key => key !== CACHE_NAME)
+            .map(key => caches.delete(key))
+      );
     })
   );
   self.clients.claim();
@@ -28,6 +31,8 @@ self.addEventListener("activate", event => {
 
 self.addEventListener("fetch", event => {
   event.respondWith(
-    caches.match(event.request).then(response => response || fetch(event.request))
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
   );
 });
